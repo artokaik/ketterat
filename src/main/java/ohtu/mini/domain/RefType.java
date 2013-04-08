76e4e50ca18e7884119967ea56d2bcff148e6fcd
Pekka Laakso <pekka.laakso@cs.helsinki.fi>
2013-04-08 12:25:31 +0300

    Added domains

---
+++ src/main/java/ohtu/mini/domain/RefType.java
@@ -0,0 +1,34 @@
+package ohtu.mini.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class RefType implements Serializable {
+
+	@Id
+	@Column(name="id")
+	private long id;
+
+	@Column(name="type_name", length = 99)
+	private String typeName;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTypeName() {
+		return typeName;
+	}
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+
+}

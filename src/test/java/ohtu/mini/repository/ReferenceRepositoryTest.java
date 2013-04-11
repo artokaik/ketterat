94c9ff694c42d478b95f553f0b96ff625ec85945
Lydia Lairala <lydia.lairala@helsinki.fi>
2013-04-11 09:58:21 +0300

    Yksikkötestit domainille ja repolle.

--- src/test/java/ohtu/mini/repository/ReferenceRepositoryTest.java
+++ src/test/java/ohtu/mini/repository/ReferenceRepositoryTest.java
@@ -35,7 +35,6 @@ public class ReferenceRepositoryTest {
     public void setUp() {
         ref = new Reference();
         repo.deleteAll();
-        System.out.println("aloitus");
     }
 
     @After
